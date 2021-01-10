567d739d1a8cb717e80e2ca36d577d0e02ed7e78
kjkrupal <kjkrupal@gmail.com>
2021-01-10 17:15:22 -0500

    celery initial

--- order-service/server/ecommerce/ecommerce/settings.py
+++ order-service/server/ecommerce/ecommerce/settings.py
@@ -142,3 +142,7 @@ ALLOWED_HOSTS = ["*"]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "../static")
 STATIC_URL = "/staticfiles/"
+
+# Celery properties
+CELERY_BROKER_URL = "amqp://guest:guest@rabbitmq:5672//"
+CELERY_RESULT_BACKEND = "django-db"
